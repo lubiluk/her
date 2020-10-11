fc17468d74e34397f7d2513a2d4603e83f701ce6
Paul Gajewski <lubiluk@gmail.com>
2020-10-12 00:32:56 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from spinup import td3_pytorch
+import torch as th
+import gym
+
+env_fn = lambda : gym.wrappers.FlattenObservation(gym.wrappers.FilterObservation(gym.make('FetchPush-v1'), ['observation', 'desired_goal']))
+
+ac_kwargs = dict(hidden_sizes=[512, 512, 64], activation=th.nn.ReLU)
+
+logger_kwargs = dict(output_dir='data', exp_name='test_0')
+
+td3_pytorch(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=5000, epochs=250, logger_kwargs=logger_kwargs)
